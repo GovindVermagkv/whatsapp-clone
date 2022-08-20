6e8d08ab572fb2f4a452e7ae1f4385d3046f475b
Govind Verma <100123540+GovindVermagkv@users.noreply.github.com>
2022-08-20 20:16:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
-var inputtext= document.getElementById("input");
+var inputtext = document.getElementById("input");
 var send = document.getElementById("send");
 var emoji = document.getElementById("emoji");
 var three_dot1 = document.getElementById("three_dot1");
-// var pop_for_threedot = document.getElementById("pop_for_threedot");
 var namevalue = document.getElementById("namevalue");
-var contact_list=document.querySelector(".contact-list")
-var contact_name=document.querySelector("#titlenm")
-var one=document.querySelector(".one")
-var two=document.querySelector(".two")
-var menu2=document.querySelector("#menu2")
-var search_icon=document.querySelector("#search_icon")
-var search_box_hidden=document.querySelector(".search-box-hidden")
+var contact_list = document.querySelector(".contact-list")
+var contact_name = document.querySelector("#titlenm")
+var one = document.querySelector(".one")
+var two = document.querySelector(".two")
+var menu2 = document.querySelector("#menu2")
+var search_icon = document.querySelector("#search_icon")
+var search_box_hidden = document.querySelector(".search-box-hidden")
+var profile_btn = document.querySelector("#profile_show")
+var profile = document.querySelector(".profile")
+var hide_profile = document.querySelector("#hide_profile")
+var settings = document.querySelector("#settings")
+var settings = document.querySelector("#settings")
+var settings_show = document.querySelector(".settings")
+var hide_settings = document.querySelector("#hide_settings")
+
+var status_edit = document.querySelector("#status-icon")
+var mobile_edit = document.querySelector("#mobile-icon")
+var email_edit = document.querySelector("#email-icon")
+var website_edit = document.querySelector("#website-icon")
+
+var status_update = document.querySelector("#status")
+var mobile = document.querySelector("#mobile")
+var email = document.querySelector("#email")
+var website = document.querySelector("#website")
 
 
 
-var list1=[];
+
+
+
+var list1 = [];
 // var list2=[];
-let n=0;
-let x=1;
-send.onclick =  function AddRow(){
-    let addRow=document.getElementById("show-chat");
-    let newRow=addRow.insertRow(n);
+let n = 0;
+let x = 1;
+send.onclick = function AddRow() {
+    let addRow = document.getElementById("show-chat");
+    let newRow = addRow.insertRow(n);
 
     list1[x] = inputtext.value;
     // list2[x] = inputtext.value;
@@ -31,7 +50,7 @@ send.onclick =  function AddRow(){
     cell1.innerHTML = list1[x]
     // cell2.innerHTML = list2[x]
 
-    inputtext.value="";
+    inputtext.value = "";
     console.log(n);
     n++;
     x++;
@@ -39,50 +58,127 @@ send.onclick =  function AddRow(){
 // n++;
 // x++;
 
-emoji.onclick = function sticker(){
+emoji.onclick = function sticker() {
     alert("Kindly check the relevent code for your Emoji and paste the code in the text area . Also ADD (&#) before the code")
 }
 
 
-contact_list.onclick= function titlename(){
+contact_list.onclick = function titlename() {
     console.log(namevalue.textContent);
     contact_name.textContent = namevalue.textContent;
 
 }
 
 
-three_dot1.onclick = function(){
+three_dot1.onclick = function () {
     console.log("menu option clicked");
-    if(one.style.display =="none"){
+    if (one.style.display == "none") {
         console.log("if targated");
-        one.style.display="block";
-        three_dot1.style.color="white"
+        one.style.display = "block";
+        three_dot1.style.color = "white"
     }
-    else{
-        one.style.display="none";
+    else {
+        one.style.display = "none";
         console.log("else targate");
-        three_dot1.style.color="black";
-    }
-}
-menu2.onclick = function(){
-    if(two.style.display =="none"){
-        two.style.display="block";
-        menu2.style.color="white";
-    }
-    else{
-        two.style.display="none";
-        menu2.style.color="black";
+        three_dot1.style.color = "black";
     }
 }
 
 
-search_icon.onclick = function(){
-    if(search_box_hidden.style.display=="none"){
-        search_box_hidden.style.display="block";
-        search_icon.style.color="white";
+
+menu2.onclick = function () {
+    if (two.style.display == "none") {
+        two.style.display = "block";
+        menu2.style.color = "white";
     }
-    else{
-        search_box_hidden.style.display="none";
-        search_icon.style.color="black";
+    else {
+        two.style.display = "none";
+        menu2.style.color = "black";
     }
 }
+
+
+search_icon.onclick = function () {
+    if (search_box_hidden.style.display == "none") {
+        search_box_hidden.style.display = "block";
+        search_icon.style.color = "white";
+    }
+    else {
+        search_box_hidden.style.display = "none";
+        search_icon.style.color = "black";
+    }
+}
+
+// -------------------------profile page show and hide -------------------------------------------//
+
+profile_btn.addEventListener("click", function () {
+    profile.style.display = "block"
+    one.style.display = "none";
+
+});
+
+hide_profile.addEventListener("click", function () {
+    profile.style.display = "none";
+})
+
+
+// ------------------------------settings page show and hide ------------------------------------//
+
+settings.addEventListener("click", function () {
+    settings_show.style.display = "block"
+    one.style.display = "none";
+
+});
+
+hide_settings.addEventListener("click", function () {
+    settings_show.style.display = "none";
+})
+
+
+// ---------------------------profile edit functions ---------------------------------------------------------//
+
+status_edit.addEventListener("click", function () {
+    if (status_update.contentEditable === "false") {
+        status_update.contentEditable = true;
+        status_edit.style.color = "blue";
+    }
+    else {
+        status_update.contentEditable = "false";
+        status_edit.style.color = "black";
+    }
+})
+
+
+mobile_edit.addEventListener("click", function () {
+    if (mobile.contentEditable === "false") {
+        mobile.contentEditable = true;
+        mobile_edit.style.color = "blue";
+    }
+    else {
+        mobile.contentEditable = "false";
+        mobile_edit.style.color = "black";
+    }
+})
+
+
+email_edit.addEventListener("click", function () {
+    if (email.contentEditable === "false") {
+        email.contentEditable = true;
+        email_edit.style.color = "blue";
+    }
+    else {
+        email.contentEditable = "false";
+        email_edit.style.color = "black";
+    }
+})
+
+website_edit.addEventListener("click", function () {
+    if (website.contentEditable === "false") {
+        website.contentEditable = "true";
+        website_edit.style.color = "blue";
+    }
+    else {
+        website.contentEditable = "false";
+        website_edit.style.color = "black";
+    }
+})
